c4a1f7e9705adc9051bc4e3adbddeebf9ff88125
Andrew Levin <andrew.michael.levin@cern.ch>
2018-11-08 22:58:23 +0100

    change the argument parsing such that the filename must immediately follow -f

--- scripts/replace_parameter.sh
+++ scripts/replace_parameter.sh
@@ -2,25 +2,28 @@
 
 usage() {
 
-	echo './replace_parameter.sh [-f] <REGISTER> <LINK> <VALUE/FILENAME>'
+    echo './replace_parameter.sh <REGISTER> <VALUE> <LINK>'
+	echo './replace_parameter.sh -f <FILENAME> <REGISTER> <LINK>'
 	echo ''
 	echo '	REGISTER - register to be updated dropping the "CFG_" substring'
 	echo ''
-	echo '	VALUE/FILENAME - value in decimal to be assigned to REGISTER or, if the -f option is used, filename containing a list of vfat number/ value pairs'
+	echo '	VALUE - value in decimal to be assigned to REGISTER'
+    echo ''
+    echo '	FILENAME - name of a file containing a list of vfat number/value pairs'
 	echo ''
 	echo '	Examples:'
 	echo ''
 	echo '		./replace_parameter.sh PULSE_STRETCH 4 1'
-	echo '		./replace_parameter.sh -f PULSE_STRETCH 4 /path/to/NominalDacValues.txt'
+	echo '		./replace_parameter.sh -f /path/to/NominalDacValues.txt PULSE_STRETCH 1'
 }
 
 ISFILE=0;
 OPTIND=1
-while getopts "fwhd" opts
+while getopts "f:h" opts
 do
     case $opts in
         f)
-            ISFILE=1;;
+            FILENAME=$OPTARG;;
         h)
             usage
             exit;;
@@ -34,26 +37,27 @@ do
 done
 unset OPTIND
 
-if (($ISFILE == 1)); then
+if [ -z ${FILENAME} ]; then
+    if [ -z ${3+x} ]
+    then
+	    usage
+        echo ""
+        echo "        The -f flag was not provided, so the first usage example above should be followed."
+        exit
+    fi
+    REGISTER=$1
+    VALUE=$2
+    LINK=$3
+    sed -i "s|^${REGISTER}.*|${REGISTER}   ${VALUE}|g" /mnt/persistent/gemdaq/vfat3/config_OH${LINK}_VFAT*_cal.txt
+else
     if [ -z ${4+x} ]
     then
 	    usage
-        echo "The -f flag was used, so the third argument should be a filename."
+        echo ""
+        echo "        The -f flag was provided, so the second usage example above should be followed."
         exit
     fi
     REGISTER=$2
     LINK=$3
-    FILENAME=$4
     awk '{system("sed -i \"s|^'$REGISTER'.*|'$REGISTER'   "$2"|g\" /mnt/persistent/gemdaq/vfat3/config_OH'$LINK'_VFAT"$1"_cal.txt")}' $FILENAME
-else
-    if [ -z ${3+x} ]
-    then
-	    usage
-        echo "The -f flag was not used, so the third argument should be a value."
-        exit
-    fi
-    REGISTER=$1
-    LINK=$2
-    VALUE=$3
-    sed -i "s|^${REGISTER}.*|${REGISTER}   ${VALUE}|g" /mnt/persistent/gemdaq/vfat3/config_OH${LINK}_VFAT*_cal.txt
 fi
