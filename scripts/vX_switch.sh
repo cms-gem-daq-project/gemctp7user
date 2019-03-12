d48bdc59f0c1d7a703ddecd50822fd6a2c556d66
Andrew Levin <amlevin@cern.ch>
2019-03-12 12:47:47 +0100

    add script to switch between v2 and v3 electronics

---
+++ scripts/vX_switch.sh
@@ -0,0 +1,30 @@
+usage() { 
+
+    echo './vX_switch.sh [vX]'
+    echo ''
+    echo '    vX = v2 or v3'
+    echo ''
+    echo 'Will change the symlinks on the ctp7 to point to the correct files for vX electronics. Ends with a call to recover.sh'
+    kill -INT $$
+}
+
+VX=$1
+
+if [ -z ${1+x} ]
+then
+    usage
+fi
+
+if [ $VX != "v2" ]  &&  [ $VX != "v3" ]
+then
+    usage
+fi   
+
+echo "Updating symlinks"
+ln -s /mnt/persistent/gemdaq_${VX}/ /mnt/persistent/gemuser/gemdaq_USER
+ln -s /mnt/persistent/rpcmodules_${VX}/ /mnt/persistent/gemuser/rpcmodules_USER
+echo "Finished symlinks"
+
+echo "Running recover.sh"
+recover.sh
+echo "Finished running recover.sh"
